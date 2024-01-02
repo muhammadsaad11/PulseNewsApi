932c2ed05a3aec1b9d022f7f175995cdb634ca01
Muhammad Saad <ms05496@st.habib.edu.pk>
2024-01-02 12:42:47 +0500

    News api files

---
+++ fetchAndSaveNews.py
@@ -0,0 +1,31 @@
+import requests
+import schedule
+import time
+from datetime import datetime
+
+def hit_api_and_save():
+    # Replace 'YOUR_API_ENDPOINT' with the actual API endpoint you want to hit
+    api_url = 'https://newsapi.org/v2/everything?q=technology+OR+AI+OR+startup+OR+Silicon+Valley&sortBy=popularity&apiKey=e8f37b515ebb44d9a0b038c6e3c19361'
+    
+    try:
+        # Make the GET request
+        response = requests.get(api_url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Save the response to a file (you can change the file name)
+            with open(f'response_news.json', 'w', encoding='utf-8') as file:
+                file.write(response.text)
+            print(f"API request successful at {datetime.now()}")
+        else:
+            print(f"API request failed with status code {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Schedule the job to run every day at a specific time (change 'HH:MM' to your desired time)
+schedule.every().day.at('18:50').do(hit_api_and_save)
+
+# Run the scheduling loop
+while True:
+    schedule.run_pending()
+    time.sleep(1)
