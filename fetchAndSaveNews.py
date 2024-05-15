d29ad094bda6cdd7b47a1e268ed9fd1163b99258
Muhammad Saad <ms05496@st.habib.edu.pk>
2024-05-15 15:37:47 +0500

    updated news parameters to remove irrelevant news

--- fetchAndSaveNews.py
+++ fetchAndSaveNews.py
@@ -17,7 +17,9 @@ def hit_api_and_save():
 
     # api_url = 'https://newsapi.org/v2/everything?q=technology&sortBy=publishedAt&apiKey=e8f37b515ebb44d9a0b038c6e3c19361'
     
-    api_url = 'https://newsapi.org/v2/everything?q=technology+AND+AI&sortBy=publishedAt&language=en&apiKey=e8f37b515ebb44d9a0b038c6e3c19361'
+    # api_url = 'https://newsapi.org/v2/everything?q=technology+AND+AI&sortBy=publishedAt&language=en&apiKey=e8f37b515ebb44d9a0b038c6e3c19361'
+
+    api_url = 'https://newsapi.org/v2/everything?q=(technology+AND+AI)+NOT+(Celebrity+AND+Actor+AND+Actress+AND+Film+AND+Star+AND+Hollywood+AND+Award+AND+Shows+AND+Grammy+AND+Oscar+AND+Met+AND+Gala+AND+Emmy+AND+Religion+AND+Religious+AND+leaders+AND+Genocide+AND+Crime+AND+Stalker+AND+Weapon+AND+Babies)&sortBy=publishedAt&language=en&apiKey=e8f37b515ebb44d9a0b038c6e3c19361'
 
     try:
         # Make the GET request
